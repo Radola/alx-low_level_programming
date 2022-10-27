e49b544d4d8b4eef94e322d81c89a812a4974725
Radola <radyahaboderin@gmail.com>
2022-10-26 17:51:58 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - Entry
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char V;
+
+	for (V = 'z'; V <= 'a'; V--)
+		putchar(V);
+	putchar('\n');
+
+	return (0);
+}
