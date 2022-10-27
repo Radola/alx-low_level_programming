d06749f0ca80c52b18390a921c107517864df341
Radola <radyahaboderin@gmail.com>
2022-10-27 00:41:30 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - Entry
+  * Return: 0
+*/
+int main(void)
+{
+	int o, p;
+
+	for (o = 0; o < 99; o++)
+	{
+		for (p = o + 1; p <= 99; p++)
+		{
+			putchar('0' + o / 10);
+			putchar('0' + o % 10);
+
+			putchar(' ');
+
+			putchar('0' + p / 10);
+			putchar('0' + p / 10);
+
+			if (o == 98 && p == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
