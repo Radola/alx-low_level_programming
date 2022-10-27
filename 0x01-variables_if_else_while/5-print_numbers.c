56f200878a839942c2b30d57a123e943d335c2a1
Radola <radyahaboderin@gmail.com>
2022-10-26 17:33:35 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return:  returns to 0
+  */
+int main(void)
+{
+	char T;
+
+	for (T = '0'; T <= '9'; T++)
+	{
+		putchar (T);
+	}
+	putchar ('\n');
+
+	return (0);
+}
