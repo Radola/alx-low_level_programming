c9ca31b31bf7e808a30623caf3ea47ab717ea7dd
Radola <radyahaboderin@gmail.com>
2022-10-26 23:06:41 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - entry
+  *
+  * Return: returns to 0
+  */
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+return (0);
+}
