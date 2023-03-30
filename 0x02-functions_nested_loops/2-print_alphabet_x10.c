2e78b4e67a45e0478ba0b2d41b3080134833f131
Radola <radyahaboderin@gmail.com>
2023-03-30 15:37:35 -0700

    0-isupper.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,11 @@
 
 /**
   * print_alphabet_x10.c - prints all alphabet 1o times in a new line
-  * Return: returns 1
   */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-
-	char j, k;
+	int j, k;
 
 		for (k = 0; k < 10; k++)
 		{
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
-/**
-  * _putchar - writes character c to stdout
-  * @c: char to be printed
-  * Return: On success 1
- */
 
-  int _putchar(char c)
+
+/**
+   * _putchar - writes the character c to stdout
+   * @c: The character to print
+   *
+   * Return: On success 1.
+   * On error, -1 is returned, and errno is set appropriately.
+  */
+
+int _putchar(char c)
 {
-	return (write(l, &c, l));
+	return (write(1, &c, 1));
 }
